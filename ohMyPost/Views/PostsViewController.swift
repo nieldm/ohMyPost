bf87811d874e1f0a892d44e04e7007bef41c0692
nieldm <nieldm@gmail.com>
2018-11-04 13:23:42 -0500

    Added and Show PostsViewController (No UI)

---
+++ ohMyPost/Views/PostsViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Then
+
+class PostsViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Posts"
+        self.view.do {
+            $0.backgroundColor = .blue
+        }
+    }
+
+}

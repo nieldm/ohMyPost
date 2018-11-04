4840de01a77b94f4a8bf419e372e4c45855f8bdf
nieldm <nieldm@gmail.com>
2018-11-04 18:03:12 -0500

    Improved PostCell UI

--- ohMyPost/Views/Posts/PostTableViewCell.swift
+++ ohMyPost/Views/Posts/PostTableViewCell.swift
@@ -18,7 +18,7 @@ class PostTableViewCell: UITableViewCell {
             $0.snp.makeConstraints { make in
                 make.edges.equalToSuperview().inset(8)
             }
-            $0.layer.cornerRadius = 4
+            $0.layer.addBorderAndShadow()
         }
         
         self.iconImageView = UIImageView(frame: CGRect.zero).then {
@@ -26,7 +26,7 @@ class PostTableViewCell: UITableViewCell {
             $0.snp.makeConstraints { make in
                 make.size.equalTo(32)
                 make.centerY.equalToSuperview()
-                make.left.equalToSuperview().offset(16)
+                make.left.equalToSuperview().offset(8)
             }
             $0.backgroundColor = .blue
         }
@@ -34,10 +34,12 @@ class PostTableViewCell: UITableViewCell {
         self.titleLabel = UILabel(frame: CGRect.zero).then {
             self.contentView.addSubview($0)
             $0.snp.makeConstraints { make in
+                make.top.equalToSuperview().offset(8)
                 make.bottom.equalTo(self.contentView.snp.centerY)
-                make.left.equalTo(self.iconImageView.snp.right).offset(4)
-                make.right.equalToSuperview()
+                make.left.equalTo(self.iconImageView.snp.right).offset(8)
+                make.right.equalToSuperview().inset(8)
             }
+            $0.numberOfLines = 2
             $0.text = "Title"
             $0.font = UIFont.OMPTitle
             $0.textColor = UIColor.dusk
@@ -47,9 +49,10 @@ class PostTableViewCell: UITableViewCell {
             self.contentView.addSubview($0)
             $0.snp.makeConstraints { make in
                 make.top.equalTo(self.contentView.snp.centerY)
-                make.left.equalTo(self.iconImageView.snp.right).offset(4)
-                make.right.equalToSuperview()
+                make.left.equalTo(self.iconImageView.snp.right).offset(8)
+                make.right.equalToSuperview().inset(8)
             }
+            $0.numberOfLines = 2
             $0.text = "SubTitle"
             $0.font = UIFont.OMPSubTitle
             $0.textColor = UIColor.wisteria
@@ -57,10 +60,14 @@ class PostTableViewCell: UITableViewCell {
     }
     
     func set(data: Post) {
-        self.titleLabel.text = data.title
+        self.titleLabel.text = data.title.capitalized
         self.subTitleLabel.text = data.body
         self.iconImageView.isHidden = !data.unread()
     }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {}
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {}
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")

fc91b3db76aba07b5811e5dee20e54b352f89f18
nieldm <nieldm@gmail.com>
2018-11-04 12:35:16 -0500

    Added new CocoaFramework

---
+++ ohMyPostBase/ohMyPostBase.h
@@ -0,0 +1,19 @@
+//
+//  ohMyPostBase.h
+//  ohMyPostBase
+//
+//  Created by Daniel Mendez on 11/4/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ohMyPostBase.
+FOUNDATION_EXPORT double ohMyPostBaseVersionNumber;
+
+//! Project version string for ohMyPostBase.
+FOUNDATION_EXPORT const unsigned char ohMyPostBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ohMyPostBase/PublicHeader.h>
+
+
